00f1107d5ba74adf3d932bbbbe88ae6aff9dde71
Aaron Pentz <AaronPentz@Aarons-MacBook-Pro.local>
2015-07-02 06:36:51 -0700

    Added notes to file.

--- word_frequency_aaron.py
+++ word_frequency_aaron.py
@@ -1,4 +1,8 @@
 import re
 from collections import Counter
+
+#open the txt file, read all, lowercase it, stirp it and find all the words in it.
 words = re.findall(r'\w+', open('sample.txt').read().lower())
+
+#Print out the 20 most common words in the file
 print(Counter(words).most_common(20))
